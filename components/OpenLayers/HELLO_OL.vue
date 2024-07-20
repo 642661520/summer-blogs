f2b4c89c47d009f2449c70675e24025a51f7a11d
finalsummer <642661520@qq.com>
2024-07-20 23:14:54 +0800

    feat: 添加OpenLayers示例组件和相关文档

---
+++ components/OpenLayers/HELLO_OL.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import "ol/ol.css";
+import Map from "ol/Map";
+import OSM from "ol/source/OSM";
+import TileLayer from "ol/layer/Tile";
+import View from "ol/View";
+const map_ref = ref();
+const map = new Map({
+  layers: [
+    new TileLayer({
+      source: new OSM({}),
+    }),
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+    minZoom: 2,
+  }),
+});
+
+onMounted(() => {
+  // 设置Target
+  map.setTarget(map_ref.value);
+});
+</script>
+
+<template>
+  <div ref="map_ref" style="width:100%;height:500px"></div>
+</template>
---
+++ docs/02.OpenLayers/index.md
@@ -0,0 +1,10 @@
+---
+title: OpenLayers
+---
+
+# OpenLayers
+
+[OpenLayers](https://openlayers.org/) 是一个高性能、功能丰富的开源地图引擎，支持多种地图数据源，提供了丰富的地图交互功能。
+<div>
+<HELLO_OL/>
+</div>
