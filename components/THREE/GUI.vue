236a79224b142c0c0b4061129b34a40237f98c85
finalsummer <642661520@qq.com>
2024-07-25 18:53:56 +0800

    feat: 添加THREE.js GUI组件和相关文档

---
+++ components/THREE/GUI.vue
@@ -0,0 +1,76 @@
+<template>
+  <div ref="dom" :style="style">
+  </div>
+</template>
+
+<script setup lang="ts">
+import * as THREE from 'three';
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import { onMounted, ref } from 'vue';
+
+const dom = ref<HTMLElement>();
+
+const width = 600;
+
+const height = 400;
+
+const style = {
+  position: 'relative',
+  width: `${width}px`,
+  height: `${height}px`
+}
+
+// 创建场景
+const scene = new THREE.Scene();
+// 创建相机
+const camera = new THREE.PerspectiveCamera(
+  75, // 视野角度
+  width / height,// 宽高比
+  0.1,// 近平面
+  1000// 远平面
+);
+
+// 设置相机位置
+camera.position.set(0, 0, 5);
+
+// 创建渲染器
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+
+// 创建几何体
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+// 创建材质
+const material = new THREE.MeshBasicMaterial({
+  color: 0x00ff00,
+});
+
+// 创建网格
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+const gui = new GUI();
+gui.domElement.style.position = 'absolute';
+const cubeFolder = gui.addFolder('Cube');
+cubeFolder.add(cube.position, 'x', -4, 4);
+
+onMounted(() => {
+  if (dom.value) {
+    // 将渲染器的dom元素添加到页面中
+    dom.value.appendChild(renderer.domElement);
+    dom.value.appendChild(gui.domElement);
+  }
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+  // 旋转立方体
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  // 渲染场景
+  renderer.render(scene, camera);
+}
+animate();
+
+</script>
